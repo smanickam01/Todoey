63f0d6fae0414336ff9b2321fb528e08b6e353a5
Srini Manickam <Srini.Man@gmail.com>
2018-03-31 09:13:38 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Srini Manickam on 3/31/18.
+//  Copyright © 2018 Srini Manickam. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
